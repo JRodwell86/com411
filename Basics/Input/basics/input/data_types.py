3c50736a5c87ef3746b9ecdb033726c534d96a8b
Jamie Rodwell <5rodwj34@solent.ac.uk>
2021-10-01 23:00:54 +0100

    Added code to demonstrate the use of different data types.

--- Basics/Input/basics/input/data_types.py
+++ Basics/Input/basics/input/data_types.py
@@ -1,5 +1,11 @@
 # Ask user to enter their name
-print("What is your name human?")
-name = input()
+name = input("What is your name human?")
 print(f"It is nice to meet you human {name}")
 # Ask user to enter their age
+age = input("How old are you (in years)?")
+Height =float(input("How tall are you (in metres)?"))
+Weight =int(input("How much do you weigh (in kilograms)?"))
+answer = Weight / Height **2
+bmi = str(round(answer, 2))
+print(f"{name} you are {age} years old and your bmi is {bmi}")
+

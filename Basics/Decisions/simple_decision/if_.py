f99dcb6ddbd83345fa4fa494589d34b3745f4338
Jamie Rodwell <5rodwj34@solent.ac.uk>
2021-10-06 19:22:42 +0100

    Added code to demonstrate the use of an if statement.

---
+++ Basics/Decisions/simple_decision/if_.py
@@ -0,0 +1,6 @@
+# check whether first number is bigger than the second number.
+# Assume first_number and second_number are existing variables.
+first_number = int(input("Enter a first number"))
+second_number = int(input("Enter a second number"))
+if first_number > second_number:
+    print("First is bigger!")

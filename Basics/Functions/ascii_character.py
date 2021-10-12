36d55a2a4825222085c5da8ef656cb183f809ae8
Jamie Rodwell <5rodwj34@solent.ac.uk>
2021-10-12 20:01:19 +0100

    Simple solutionadded to ascii characters

--- Basics/Functions/ascii_character.py
+++ Basics/Functions/ascii_character.py
@@ -12,3 +12,15 @@ else:
     print(f"This code is not in range 32-126")
 print()
 print("Program Ended!")
+
+#print("Program Started!")
+
+#print("Please enter an ASCII code:")
+#code = int(input())
+
+#if code >= 32 and code <= 126:
+ #   print(f"The character represented by the ASCII value {code} is: {chr(code)}")
+#else:
+ #   print("The character cannot be displayed.")
+
+#print("Program Ended!")

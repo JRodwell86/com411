79941efc6991e6bd077e1cf2226e4296fd10f136
Jamie Rodwell <5rodwj34@solent.ac.uk>
2021-10-28 20:18:57 +0100

    update to iterate_list

--- data/lists/iterate_list.py
+++ data/lists/iterate_list.py
@@ -5,9 +5,10 @@ def directions():
 def menu():
     print("Please select a direction:")
     dirs = directions()
+#code to identify index points of a list
     for index in range(len(dirs)):
-        movement =dirs[index]
-        print(f"{index}: {movement}")
+        #code to show index number and the list items from that index position.
+        print(f"{index}: {dirs[index]}")
 
 def run():
     menu()

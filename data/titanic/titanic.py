4777cca62b6b5ce33f521aceb34ff2b0a778aefa
Jamie Rodwell <5rodwj34@solent.ac.uk>
2021-10-29 21:48:12 +0100

    Added code to display the number of each gender.

--- data/titanic/titanic.py
+++ data/titanic/titanic.py
@@ -42,6 +42,18 @@ def display_num_survivors():
             num_survived += 1
     print(f"{num_survived} passengers survived.")
 
+def display_passengers_per_gender():
+    females = 0
+    males = 0
+
+    for record in records:
+        gender = (record[4])
+        if gender == "male":
+            males += 1
+        else:
+            females += 1
+    print(f"Females: {females} Males: {males}")
+
 
 
 def run():
@@ -53,6 +65,8 @@ def run():
         display_passenger_names()
     elif selected_option == 2:
         display_num_survivors()
+    elif selected_option == 3:
+        display_passengers_per_gender()
     else:
         print("Error! Option not recognised")
 

87cf172e9d77451b341fa41d795c9d18ab1bf93c
Jamie Rodwell <5rodwj34@solent.ac.uk>
2021-10-29 21:38:01 +0100

    update to count letters

--- basics/repetitions/for_loop/count_letters.py
+++ basics/repetitions/for_loop/count_letters.py
@@ -3,5 +3,8 @@ def run():
     user_word = input()
     count = 0
     for position in range(0, len(user_word), 1):
-        count = count + 1
+        count += 1
     print(count)
+
+if __name__ == "__main__":
+  run()

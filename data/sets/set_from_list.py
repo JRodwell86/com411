b64053ba0bdf89a28e2c3710311ac45b998663c0
Jamie Rodwell <5rodwj34@solent.ac.uk>
2021-11-05 11:00:03 +0000

    Added code to demonstrate how to create a set from a list

--- data/sets/set_from_list.py
+++ data/sets/set_from_list.py
@@ -3,21 +3,29 @@ def observed():
     observations = []
     #while loop to select how many times to ask the question
     while len(observations) <= 6:
-
-        observation = (input("Please enter an observation"))
-        observations.append(observation)
-
+#code to add answers to question straight to the list
+        print("Please enter an observation")
+        observations.append(input())
+#returning the answers to the list and therefore the function
+    return observations
 
 
 def run():
     print("Counting observations...")
     #returning first function in a local variable
-    seen = observed()
+    observations = observed()
     #creating an empty set
-    seen_set = set()
-    for seen_set in seen:
-        seen_set.count("Skyscraper")
-    print(seen_set)
+    observations_set = set()
+    #observation is a new variable for this for loop
+    for observation in observations:
+    #create a variable to store the info in. So reads the variable and second part counts how many times variable appears
+        data = (observation, observations.count(observation))
+    #add the data that i've found to the blank set
+        observations_set.add(data)
+    #data variable stored in the observations_set
+    for data in observations_set:
+    #print the positions of the data in the set. 0 = observation name and 1 = times mentioned
+        print(f"{data[0]} observed {data[1]} times.")
 
 if __name__ == "__main__":
   run()

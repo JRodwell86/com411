1f18ac78f93160a79e2bfb5930e40ed54e9e1c37
Jamie Rodwell <5rodwj34@solent.ac.uk>
2021-11-05 14:16:45 +0000

    Added code to demonstrate how to iterate through a dictionary.

--- data/dicts/iterate_dict.py
+++ data/dicts/iterate_dict.py
@@ -8,16 +8,28 @@ def pattern():
 
 
 def display_keys(data):
+    print("Keys:")
 #key is an identifier - will always return the 'key' - data is the returned dictionary.
     for key in data:
-
         print(key)
+
+def display_values(data):
+    print("\nValues:")
+    for value in data.values():
+        print(value)
+
+def display_items(data):
+    print("\nPairs:")
+    for key, value in data.items():
+        print(f"{key}, {value}")
 
 def run():
     #create variable from the first function
     data = pattern()
     #put that variable (dictionary) in to the second function
     display_keys(data)
+    display_values(data)
+    display_items(data)
 
 if __name__ == "__main__":
   run()

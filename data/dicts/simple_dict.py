af3731914320ad5de89332e36d22de7c5f27d56c
Jamie Rodwell <5rodwj34@solent.ac.uk>
2021-11-05 11:37:44 +0000

    added comments to simple_dict

--- data/dicts/simple_dict.py
+++ data/dicts/simple_dict.py
@@ -1,11 +1,13 @@
 def pattern():
-
+#blank dictionary
     sequences = {}
+#adding contents of dictionary
     sequences = {"Short Sequence":3, "Medium Sequence":2, "Long Sequence": 1}
+#returning the dict to the function
     return sequences
 
 def run():
-
+#code to print the dict from the function
     print(pattern())
 
 if __name__ == "__main__":

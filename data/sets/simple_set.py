22cbd5ba20271aef44da4a668bc1366550f319d8
Jamie Rodwell <5rodwj34@solent.ac.uk>
2021-11-05 10:13:18 +0000

    Added code to demonstrate a simple set

---
+++ data/sets/simple_set.py
@@ -0,0 +1,16 @@
+def observed():
+    #creating blank set
+    observations = set()
+
+    #populating the set
+    observations = {"Flying Car", "Sky Scraper", "Sky Scraper", "Laser", "Dome", "Dome"}
+
+    #return set
+    print(observations)
+
+def run():
+    observed()
+
+
+if __name__ == "__main__":
+  run()

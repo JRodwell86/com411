158c0a4090058dd44d475c81961f3aa3489303a7
Jamie Rodwell <5rodwj34@solent.ac.uk>
2021-11-09 21:47:03 +0000

    updated olympics comments

--- data/olympics/process.py
+++ data/olympics/process.py
@@ -40,22 +40,30 @@ def tally_medals(data):
 #calls a function to print off an end portion of the display
     tui.completed()
 
-
+#data is athelete_events and added in the main
 def tally_team_medals(data):
+    # calling the function and entering the parameter if this one is chose
     tui.started("Tallying medals for each team")
+#empty list for medal tally
     medal_tally = {}
+#for a new variable in athelete events
     for record in data:
+##variable is the data[column in data]
         team = record[COL_TEAM]
         medal = record[COL_MEDAL]
-
+#if the medal (identified line by line in data above) is not inone of the below
         if medal not in ("Gold", "Silver", "Bronze"):
+#if there is no info for gold, silver, bronze in the medal variable then skip over the rest and start loop again
             continue
-
+#if team (from the data column) is in blank variable medal tally. To be in the blank list it would be created in the else below. So first time there would be nothing and it would be added. second time it would just add to exisiting.
         if team in medal_tally:
+# if medal is gold, silver, bronze then a new list is created of team name + medal. first and second variable are the key pairs.
             medal_tally[team][medal] += 1
         else:
+#this statement creates the list for the above if statement to then fill in.
             medal_tally[team] = {"Gold": 0, "Silver": 0, "Bronze": 0}
             medal_tally[team][medal] += 1
-
+    # adds the list as a parameter to the function
     tui.display_team_medal_tally(medal_tally)
+    # calls a function to print off an end portion of the display
     tui.completed()

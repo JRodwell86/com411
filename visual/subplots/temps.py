4e2f6644da3bbc6c731b963b5d746d4b6b35e078
Jamie Rodwell <5rodwj34@solent.ac.uk>
2021-11-19 10:08:57 +0000

    updates

---
+++ visual/subplots/temps.py
@@ -0,0 +1,36 @@
+# importing the visualisation stuff.
+import matplotlib.pyplot as plt
+
+
+# function to read data
+
+def read_data(file_path):
+    # empty list
+    temps = []
+    # opening the file in read mode
+    with open(file_path) as file:
+        # loop to iterate through txt lines
+        for line in file:
+            # adding to list as numbers and strip to remove gaps and \n
+            temps.append(int(line.strip()))
+        # return the list to the function
+        return temps
+
+
+def run():
+    # call first function and assign data to variable
+    data = read_data('temps.txt')
+    # create a figure of 2 axes
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    # assign x & y variables
+    x = range(len(data))
+    y = data
+    # create different axes from data
+    ax1.plot(x, y)
+    ax2.bar(x, y)
+    # display the graphs
+    plt.show()
+
+
+if __name__ == "__main__":
+    run()

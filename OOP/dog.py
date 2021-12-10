4191070d731b53e7323639ad351a23f212dde780
Jamie Rodwell <5rodwj34@solent.ac.uk>
2021-12-10 15:14:35 +0000

    darren oop example

---
+++ OOP/dog.py
@@ -0,0 +1,19 @@
+class Dog:
+
+    def __init__(self):
+        self.name = "fido"
+        self.energy = 50
+        self.is_good_dog = True
+        self.is_indoors = True
+
+
+first_dog = Dog()
+
+
+print(first_dog.name)
+
+
+
+
+
+

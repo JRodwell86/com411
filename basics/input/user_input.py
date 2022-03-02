28cc83a9a85f6495654f4f94a7c0211665dfaa79
Rodwell <Jamie.Rodwell@portsmouthcc.gov.uk>
2022-03-02 15:36:28 +0000

    change

--- basics/input/user_input.py
+++ basics/input/user_input.py
@@ -1,5 +1,5 @@
 # Ask user to enter their name
-def run():
-    print("What is your name human?")
-    name = input()
-    print(f"It is nice to meet you human {name}")
+
+print("What is your name human?")
+name = input()
+print(f"It is nice to meet you human {name}")
